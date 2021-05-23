62ef047a12b081b24947bb37efbfd90a226fb2b0
adityahimaone <adityahimaone@gmail.com>
2021-05-23 22:40:58 +0700

    final

---
+++ src/server.js
@@ -0,0 +1,22 @@
+/* eslint linebreak-style: ["error", "windows"]*/
+const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
+const init = async () => {
+  const server = Hapi.server({
+    // port: 5000,
+    // eslint-disable-next-line max-len
+    // host: process.env.NODE_ENV !== 'production' ? 'localhost' : '34.101.5.116',
+    port: process.env.PORT || 8080,
+    host: 'bangkit-capstone-313209.et.r.appspot.com',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+  server.route(routes);
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
